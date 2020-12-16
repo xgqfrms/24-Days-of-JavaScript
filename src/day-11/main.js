4c0bf52245b174eaed0159089015a89ea339c475
xgqfrms <xgqfrms@outlook.com>
2020-12-16 23:49:41 +0800

    Day11 Solution ✅

--- src/day-11/main.js
+++ src/day-11/main.js
@@ -1,5 +1,12 @@
 function avoidObstacles(nums) {
-  //  write code here.
+  // const largestNum = nums.sort((a, b) => a - b)[nums.length - 1];
+  const largestNum = nums.sort((a, b) => a - b > 0 ? -1 : 1)[0];
+  for (let i = 1; i <= largestNum + 1; i++) {
+    if (nums.every((value) => value % i !== 0)) {
+      // break;
+      return i;
+    }
+  }
 }
 
 
@@ -10,7 +17,7 @@ function avoidObstacles(nums) {
 describe('avoidObstacles()', () => {
   it('returns minimal number of jumps in between numbers', () => {
       // arrange
-      const nums = [5, 3, 6, 7, 9];
+      const nums = [5, 3, 6, 9, 7];
 
       // act
       const result = avoidObstacles(nums);
